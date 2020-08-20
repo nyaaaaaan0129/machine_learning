da611ce566b07dfd77baabd65235b7a204911fd9
Hanaho Murata <nyaaaaaan0129@gmail.com>
2020-08-21 04:01:24 +0900

    resolved

--- pybotweb/pybotweb.py
+++ pybotweb/pybotweb.py
@@ -1,16 +1,18 @@
 from bottle import route, run, template, request
-from datetime import datetime
-from pybot2 import pybot
+from pybot import pybot
 
 
 @route('/hello')
 def hello():
-	return template('pybot_template', input_text='', output_text='')
+    return template('pybot_template', input_text='', output_text='')
+
 
 @route('/hello', method='POST')
 def do_hello():
-    input_text = request.forms.input_text
-    output_text = pybot(input_text)
+    input_text = request.forms['input_text']
+    input_image = request.files.input_image
+    output_text = pybot(input_text, input_image)
     return template('pybot_template', input_text=input_text, output_text=output_text)
+
 
 run(host='localhost', port=8080, debug=True)
--- pybotweb/views/pybot_template.tpl
+++ pybotweb/views/pybot_template.tpl
@@ -1,10 +1,14 @@
 <html>
 <body>
-<form method="post" action="/hello">
-<input type="text" name="input_text">
+<h1>pybot Webアプリケーション</h1>
+<form method="post" action="/hello" enctype="multipart/form-data">
+メッセージを入力してください: <input type="text" name="input_text"><br>
+画像を選択してください: <input type="file" name="input_image"><br>
 <input type="submit" value="送信">
 </form>
-<br>
-送信されたテキスト: {{text}}
+<ul>
+<li>入力されたメッセージ: {{input_text}}</li>
+<li>pybotからの応答メッセージ: {{!output_text}}</li>
+</ul>
 </body>
 </html>
